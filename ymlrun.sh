d35bbda7eadd4b029eb6d8ed535b3d6fc4d62e1a
Dan Amzulescu <35110114+qualidan@users.noreply.github.com>
2021-04-29 05:37:25 +0000

    Update ymlrun.sh

--- ymlrun.sh
+++ ymlrun.sh
@@ -4,5 +4,4 @@ sudo yum install pip -y
 sudo yum install wget -y
 wget https://raw.githubusercontent.com/qualidan/public_app_cm_scripts/main/install_docker_pull_and_run_container_centos7-local.yml
 pip install ansible
-wget https://raw.githubusercontent.com/qualidan/public_app_cm_scripts/main/install_docker_pull_and_run_container_centos7-local.yml
 ansible-playbook install_docker_pull_and_run_container_centos7-local.yml
