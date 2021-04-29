59af62cc742c57c419476aba31acd32ed1592c2e
Dan Amzulescu <35110114+qualidan@users.noreply.github.com>
2021-04-29 05:36:52 +0000

    Update ymlrun.sh

--- ymlrun.sh
+++ ymlrun.sh
@@ -1,6 +1,8 @@
 sudo -i
 sudo yum install epel-release -y
 sudo yum install pip -y
+sudo yum install wget -y
 wget https://raw.githubusercontent.com/qualidan/public_app_cm_scripts/main/install_docker_pull_and_run_container_centos7-local.yml
 pip install ansible
-ansible-playbook https://raw.githubusercontent.com/qualidan/public_app_cm_scripts/main/install_docker_pull_and_run_container_centos7-local.yml
+wget https://raw.githubusercontent.com/qualidan/public_app_cm_scripts/main/install_docker_pull_and_run_container_centos7-local.yml
+ansible-playbook install_docker_pull_and_run_container_centos7-local.yml
